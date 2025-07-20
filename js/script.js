b862961d5b1a763ae219409edf430cfa1e19f1c1
unknown <joannakolbe@wp.pl>
2025-07-20 10:25:53 +0200

    Add active class to clicked link using this

--- js/script.js
+++ js/script.js
@@ -1,42 +1,33 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
+  console.log('clickedElement:', clickedElement);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
 
-  /* add class 'active' to the clicked link */
-  event.preventDefault(); // zapobiega domyślnemu zachowaniu linka (skok do elementu)
-  const clickedLink = event.currentTarget;
-  clickedLink.classList.add('active');
+  /* [DONE] add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
 
-  /* remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('article.active');
-  for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
-  }
+  /* [IN PROGRESS] remove class 'active' from all articles */
 
   /* get 'href' attribute from the clicked link */
-  const articleSelector = clickedLink.getAttribute('href');
 
   /* find the correct article using the selector (value of 'href' attribute) */
-  const targetArticle = document.querySelector(articleSelector);
 
   /* add class 'active' to the correct article */
-  if(targetArticle){
-    targetArticle.classList.add('active');
-  }
 }
 
 const links = document.querySelectorAll('.titles a');
-
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
 
 
 
