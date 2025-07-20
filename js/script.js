3f118d3c32af74f196a7005cbce8869524d7c291
unknown <joannakolbe@wp.pl>
2025-07-20 10:31:21 +0200

    Add preventDefault and highlight clicked link

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault(); // [DONE] blokuje domyślne zachowanie linków (np. przewijanie)
   const clickedElement = this;
   console.log('Link was clicked!');
   console.log('clickedElement:', clickedElement);
 
-  /* [DONE] remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links */
   const activeLinks = document.querySelectorAll('.titles a.active');
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
@@ -27,6 +28,7 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
 
 
 
