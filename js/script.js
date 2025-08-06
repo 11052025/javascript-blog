53926bdf2908755337757fa2385397de82c20def
unknown <joannakolbe@wp.pl>
2025-08-06 19:48:27 +0200

    Implement generateTags to dynamically create tag links in each article

--- js/script.js
+++ js/script.js
@@ -46,6 +46,45 @@ function generateTitleLinks() {
 // Wywołujemy funkcję generującą linki wraz z przypięciem event listenerów
 generateTitleLinks();
 
+function generateTags() {
+  /* find all articles */
+  const articles = document.querySelectorAll('.post');
+
+  /* START LOOP: for every article: */
+  for (let article of articles) {
+
+    /* find tags wrapper */
+    const tagWrapper = article.querySelector('.post-tags ul');
+
+    /* make html variable with empty string */
+    let html = '';
+
+    /* get tags from data-tags attribute */
+    const tags = article.getAttribute('data-tags');
+
+    /* split tags into array */
+    const tagsArray = tags.split(' ');
+
+    /* START LOOP: for each tag */
+    for (let tag of tagsArray) {
+
+      /* generate HTML of the link */
+      const linkHTML = `<li><a href="#tag-${tag}">${tag}</a></li>`;
+
+      /* add generated code to html variable */
+      html += linkHTML;
+    }
+    /* END LOOP: for each tag */
+
+    /* insert HTML of all the links into the tags wrapper */
+    tagWrapper.innerHTML = html;
+  }
+  /* END LOOP: for every article: */
+}
+
+generateTags();
+
+
 
 
 
